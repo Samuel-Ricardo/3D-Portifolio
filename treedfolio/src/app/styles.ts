a870fe929d7d337c443af679f23594054385965d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-25 23:53:05 -0300

    [ :art: ] | creae: style - patterns > hero (app)

--- treedfolio/src/app/styles.ts
+++ treedfolio/src/app/styles.ts
@@ -5,4 +5,7 @@ export const styles = {
 
   heroHeadText:
     "font-balck text-white lg:text-[80px] sm:text-[60px] xs:text-[50] text-[40px] lg:leading-[98px] mt-2",
+
+  heroSubText:
+    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs-text-[20px] text-[16px] lg:leading-[40px]",
 };
