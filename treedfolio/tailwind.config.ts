ebdac1b4bf6994b6977a3607a49ec60de2a6d91e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-26 18:37:17 -0300

    [ :art: ] | setup: colors (tailwind::config)

--- treedfolio/tailwind.config.ts
+++ treedfolio/tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#AAA6C3",
+        tertiary: "#151030",
+        "black-100": "#100D25",
+        "black-200": "#090325",
+        "white-100": "#F3F3F3",
+      },
       backgroundImage: {
         "hero-pattern": "url('/src/assets/herobg.png')",
       },
