0e38baa09ef21401165d03b699ce0b6c0c4473f7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-26 17:11:58 -0300

    [ :clapper: ] | create: animation - zoomIn (app::utils)

--- treedfolio/src/utils/motion.ts
+++ treedfolio/src/utils/motion.ts
@@ -41,3 +41,23 @@ export const fadeIn = (
     },
   };
 };
+
+export const zoomIn = (delay: number, duration: number) => {
+  return {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        ease: "easeOut",
+        delay,
+        duration,
+      },
+    },
+  };
+};
