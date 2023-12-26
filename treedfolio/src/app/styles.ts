5ae8626f367132d3854d73aada52023ce28672f8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-25 23:58:56 -0300

    [ :art: ] | creae: style - patterns > section (app)

--- treedfolio/src/app/styles.ts
+++ treedfolio/src/app/styles.ts
@@ -3,9 +3,21 @@ export const styles = {
   paddingY: "sm:py-16 py-6",
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
-  heroHeadText:
-    "font-balck text-white lg:text-[80px] sm:text-[60px] xs:text-[50] text-[40px] lg:leading-[98px] mt-2",
+  hero: {
+    head: {
+      text: "font-balck text-white lg:text-[80px] sm:text-[60px] xs:text-[50] text-[40px] lg:leading-[98px] mt-2",
+    },
+    sub: {
+      text: "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs-text-[20px] text-[16px] lg:leading-[40px]",
+    },
+  },
 
-  heroSubText:
-    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs-text-[20px] text-[16px] lg:leading-[40px]",
+  section: {
+    head: {
+      text: "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+    },
+    sub: {
+      text: "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+    },
+  },
 };
