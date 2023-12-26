1b57891b1aac33343f52f6a7252366653f5260fc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-25 23:51:23 -0300

    [ :art: ] | creae: style - patterns > hero (app)

--- treedfolio/src/app/styles.ts
+++ treedfolio/src/app/styles.ts
@@ -2,4 +2,7 @@ export const styles = {
   paddingX: "sm:px-16 px-6",
   paddingY: "sm:py-16 py-6",
   padding: "sm:px-16 px-6 sm:py-16 py-10",
+
+  heroHeadText:
+    "font-balck text-white lg:text-[80px] sm:text-[60px] xs:text-[50] text-[40px] lg:leading-[98px] mt-2",
 };
