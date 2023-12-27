88e1a5ddb47f8a2bef3225bd13d1d95c0fab658d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-27 13:16:48 -0300

    [ :art: ] | setup: section - contacts > handleChange (app)

--- treedfolio/src/sections/contacts.section.tsx
+++ treedfolio/src/sections/contacts.section.tsx
@@ -1,5 +1,20 @@
+"use client";
+
 import { SectionWrapper } from "@/components/wrapper/section.component";
+import { useCallback, useRef, useState } from "react";
 
 export const Contacts = SectionWrapper(() => {
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = useCallback(
+    (e: any) => {
+      const { name, value } = e.target;
+      setForm({ ...form, [name]: value });
+    },
+    [form],
+  );
+
   return <></>;
 }, "contacts");
