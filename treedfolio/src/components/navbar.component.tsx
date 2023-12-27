af8db31ad54f0dbff8a04eb7c46b2aca45224c55
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-27 11:53:04 -0300

    [ :art: ] | update: component - navbar (app)

--- treedfolio/src/components/navbar.component.tsx
+++ treedfolio/src/components/navbar.component.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { styles } from "@/app/styles";
+import { logo } from "@/assets";
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export const NavBar = () => {
-  const [activeNav, setActiveNav] = useState("");
+  const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
@@ -17,5 +21,30 @@ export const NavBar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  return <nav></nav>;
+  return (
+    <nav
+      className={`${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${
+        scrolled ? "bg-primary" : "bg-transparent"
+      }`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          href="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <Image src={logo} alt="Logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Adrian &nbsp;{" "}
+            <span className="sm:block hidden"> | JavaScript Mastery</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
 };
