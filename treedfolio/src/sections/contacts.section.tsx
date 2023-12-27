6dec8fa6e99d8e3a0f0620766c2f526027f37220
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-27 13:57:25 -0300

    [ :art: ] | setup: section - contacts > handleSubmit (app)

--- treedfolio/src/sections/contacts.section.tsx
+++ treedfolio/src/sections/contacts.section.tsx
@@ -2,6 +2,7 @@
 
 import { SectionWrapper } from "@/components/wrapper/section.component";
 import { useCallback, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export const Contacts = SectionWrapper(() => {
   const formRef = useRef();
@@ -12,6 +13,40 @@ export const Contacts = SectionWrapper(() => {
     (e: any) => {
       const { name, value } = e.target;
       setForm({ ...form, [name]: value });
+    },
+    [form],
+  );
+
+  const handleSubmit = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      setLoading(true);
+
+      emailjs
+        .send(
+          process.env.VITE_APP_EMAILJS_SERVICE_ID + "",
+          process.env.VITE_APP_EMAILJS_TEMPLATE_ID + "",
+          {
+            from_name: form.name,
+            to_name: "Samuel Ricardo",
+            from_email: form.email,
+            to_email: process.env.VITE_APP_EMAILJS_RECEIVER,
+            message: form.message,
+          },
+          process.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+        )
+        .then(
+          () => {
+            setLoading(false);
+            alert("Thank you. I will get back to you as soon as possible.");
+
+            setForm({ name: "", email: "", message: "" });
+          },
+          (error) => {
+            console.error(error);
+            alert("Ahh, something went wrong. Please try again.");
+          },
+        );
     },
     [form],
   );
