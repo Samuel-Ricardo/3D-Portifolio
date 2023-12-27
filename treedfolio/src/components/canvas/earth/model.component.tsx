60737b8b36f896730428ca3e1fc9970f3b4fae09
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-27 00:26:47 -0300

    [:recycle:] | refact: rename - component > Earth (app::canvas)

--- treedfolio/src/components/canvas/earth/model.component.tsx
+++ treedfolio/src/components/canvas/earth/model.component.tsx
@@ -2,7 +2,7 @@
 
 import { useGLTF } from "@react-three/drei";
 
-export const Earth = () => {
+export const EarthModel = () => {
   const earth = useGLTF("./planet/scene.gltf");
   return (
     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
