85c1a0432d24de0a4d33758e83ee484077c9f8d1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-27 11:44:36 -0300

    [ :eyes: ] | observe: scroll (app::components)

--- treedfolio/src/components/navbar.component.tsx
+++ treedfolio/src/components/navbar.component.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const NavBar = () => {
   const [activeNav, setActiveNav] = useState("");
   const [toggle, setToggle] = useState(false);
   const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setScrolled(scrollTop > 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return <nav></nav>;
 };
