97c997a090e6891bcee753f1670833a0769a0b5f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-26 23:44:36 -0300

    [ :art: ] | create: component - render > computer (app::canvas)

--- treedfolio/src/components/canvas/computer/render.component.tsx
+++ treedfolio/src/components/canvas/computer/render.component.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { CanvasLoader } from "@/components/loader.component";
+import { OrbitControls, Preload } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useState } from "react";
+import { ComputerModel } from "./model.component";
 
 export const Computer = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -17,4 +21,18 @@ export const Computer = () => {
     return () =>
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
   }, []);
+
+  return (
+    <Canvas>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <ComputerModel isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
 };
