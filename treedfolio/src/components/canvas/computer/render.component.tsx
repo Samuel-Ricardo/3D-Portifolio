2827c023e88ce8be1c4a11b7250ba1ab39057414
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-27 16:44:15 -0300

    [ :art: ] | update: component - render > computer (app::canvas)

--- treedfolio/src/components/canvas/computer/render.component.tsx
+++ treedfolio/src/components/canvas/computer/render.component.tsx
@@ -23,7 +23,13 @@ export const Computer = () => {
   }, []);
 
   return (
-    <Canvas>
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
